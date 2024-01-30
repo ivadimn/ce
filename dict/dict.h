170dae139ea4aac63c2bb95598c69547cd2a6f6d
vadim <ivadimn@mail.ru>
2024-01-30 05:43:08 +0300

    add dict

---
+++ dict/dict.h
@@ -0,0 +1,21 @@
+#ifndef _DICT_H_
+#define _DICT_H_
+
+#include <unistd.h>
+
+typedef struct cell_t {
+    size_t count;
+    char* word;
+} cell_t;
+
+typedef struct dict_t {
+    cell_t **data;          //данные
+    size_t count;           //
+} dict_t;
+
+
+
+
+
+
+#endif
