d53683b89db25215c53391b718bdd3a3e3ed271a
vadim <ivadimn@mail.ru>
2024-01-31 17:22:00 +0300

    new version dict

--- dict/dict.c
+++ dict/dict.c
@@ -3,8 +3,50 @@
 #include <fcntl.h>
 #include <stdint.h>
 #include <ctype.h>
+#include <locale.h>
 
-typedef char alpha;
+static const float DEFAULT_FACTOR = 0.72f;
+static const size_t DEFAULT_SIZE = 256;
+static const float MULT = 2.0f;
+
+dict_t* create_dict(size_t size, float factor, float mult) {
+    dict_t *dict = (dict_t*) malloc(sizeof(dict_t));
+    dict->count = 0;
+    dict->size = (size >= DEFAULT_SIZE) ? size : DEFAULT_SIZE;
+    dict->factor = (factor >= DEFAULT_FACTOR && factor < 1.0f ) 
+                    ? factor : DEFAULT_FACTOR;
+    dict->limit = (size_t) (dict->factor * dict->size);
+    dict->mult = (mult >= MULT) ? mult : MULT;
+    dict->data = (cell_t**) calloc(dict->size, sizeof(cell_t));                 
+    return dict;
+}
+
+void put(dict_t **dict, alpha* word) {
+    return;
+}
+
+cell_t* get_data(dict_t *dict, alpha* word) {
+    unsigned long hash = hash_code(word);
+    size_t index = hash % dict->size;
+    return NULL;
+}
+
+
+unsigned long hash_code(alpha* word) {
+    unsigned long hash = 5381;
+    unsigned long g;
+    while (*word != '\0') {
+        hash = (hash << 4) + (*word);
+        g = hash;
+        if (g & 0xf0000000)   {
+            hash = hash ^ (g >> 24);
+            hash = hash ^ g;
+        }
+        word++;
+    }
+    return hash;
+}
+
 
 void file_handle(char* filename) {
 
@@ -13,6 +55,9 @@ void file_handle(char* filename) {
     alpha *buffer; 
     alpha word[128];
     size_t idx = 0;
+    unsigned long hash;
+
+
 
     fd = open(filename, O_RDONLY);
     if (fd == -1)
@@ -33,23 +78,29 @@ void file_handle(char* filename) {
         buffer = (alpha*) malloc(reading_size);
     }
 
-    //ispunct(2);
-
+    
     if (buffer == NULL)
         err_sys("Ошибка распределения памяти.");
         
-    while (read(fd, buffer, reading_size) == reading_size) {
+    while ((reading_size = read(fd, buffer, reading_size)) > 0) {
         for (int i = 0; i < reading_size; i++) {
-           if(ispunct(buffer[i]) || isspace(buffer[i])) {
+            //printf(" %X\n", buffer[i]);
+            if ((buffer[i] >> 7) & 0x01) {
+                word[idx++] = buffer[i];
+                continue;
+            }
+            if (isalpha(buffer[i]) || buffer[i] == '\'') {  
+                word[idx++] = buffer[i];
+            }
+            else {
                 if (idx > 0)                 {
                     word[idx] = '\0';
-                    printf("Word: %s\n", word);
+                    hash = hash_code(word);
+                    printf("hash: %lu,  index: %lu, size: %lu, word: %s\n", 
+                            hash, hash % 255, strlen((char*) word), word);
                     idx = 0;    
                 }
-           }
-           else {
-                word[idx++] = buffer[i];
-           }
+            }
         }
     }
     free(buffer);
--- dict/dict.h
+++ dict/dict.h
@@ -3,17 +3,29 @@
 
 #include <unistd.h>
 
+typedef uint8_t alpha;
+
 typedef struct cell_t {
     size_t count;
-    char* word;
+    alpha* word;
 } cell_t;
 
 typedef struct dict_t {
     cell_t **data;          //данные
-    size_t count;           //
+    size_t count;           //сколько уже заполнено
+    size_t size;            //всего элементов массива
+    size_t limit;           //если элементов больше чем лимит пересоздаём dict
+    float factor;           //степень заполнения массива по 
+                            //достижении которого пересоздётся dict
+    float mult;             //во столько раз увеличится размер dict           
 } dict_t;
 
+dict_t* create_dict(size_t, float, float);
+void put(dict_t **dict, alpha* word);
+cell_t* get_data(dict_t *dict, alpha* word);
 
+
+unsigned long hash_code(alpha* word);
 void file_handle(char* filename);
 
 
