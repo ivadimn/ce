f84a73b863fdf8b5684a469717f4b0631b65a91b
vadim <ivadimn@mail.ru>
2024-02-26 18:15:23 +0300

    update asm

--- asm/main03.asm
+++ asm/main03.asm
@@ -12,103 +12,13 @@ section '.bss' writeable
 section '.text' executable
 _start:
    
-    mov rax, 571898928
+    mov rax, 321
     call print_number   ; вызываем функцию печати числа
-    ;call print_line     ; печатаем переводс строки
+    mov rax, 'G'
+    call print_char   ; вызываем функцию печати числа
+    call print_line     ; печатаем переводс строки
     call exit
-    
-section '.string_to_number' executable
-; | input
-; rax = string
-; | output
-; rax = number
-string_to_number:
-    push rbx
-    push rcx
-    push rdx
-    xor rcx, rcx
-    xor rbx, rbx
 
-    .next_iter:
-        cmp [rax + rbx], byte 0
-        je .next_step
-        mov cl, [rax + rbx]
-        sub cl, '0'
-        push rcx        
-        inc rbx
-        jmp .next_iter
-    .next_step:
-        mov rcx, 1
-        xor rax, rax
-    .to_number:
-        cmp rbx, 0
-        je .close
-        pop rdx
-        imul rdx, rcx
-        imul rcx, 10
-        add rax, rdx
-        dec rbx
-        jmp .to_number
-    .close:
-        pop rdx
-        pop rcx
-        pop rbx    
-        ret
-
-section '.number_to_string' executable
-; | input
-; | rax = number
-; | rbx = buffer     
-; | rcx = buffer.size
-number_to_string:
-    push rax
-    push rbx
-    push rcx
-    push rdx
-    push rsi
-    mov rsi, rcx
-    xor rcx, rcx
-    dec rsi
-    .next_iter:
-        push rbx
-        mov rbx, 10
-        xor rdx, rdx        ; в rdx помещается остаток отделения    
-        div rbx 
-        pop rbx                
-        add rdx, '0'         ; в div делимое всегда берётся из rax     
-        push rdx             ; сохраняем символ в стеке   
-        inc rcx
-        cmp rax, 0
-        je .next_step
-        jmp .next_iter
-    .next_step:
-        mov rdx, rcx
-        xor rcx, rcx    
-    .to_string:
-        cmp rcx, rsi
-        je .pop_iter
-        cmp rcx, rdx
-        je .null_char
-        pop rax
-        mov [rbx+rcx], rax
-        inc rcx
-        jmp .to_string
-    .pop_iter:
-        cmp rcx, rdx
-        je .close
-        pop rax
-        inc rcx
-        jmp .pop_iter
-    .null_char:
-        mov rsi, rdx        
-    .close:
-        mov [rbx + rsi], byte 0
-        pop rsi
-        pop rdx
-        pop rcx
-        pop rbx       
-        pop rax
-        ret
 
 section '.print_number' executable
 print_number:
@@ -152,7 +62,7 @@ print_char:
     push rsi
     push rdi
     push rdx
-    ;mov rax, '5'
+    
     push rax
                        ;  дял 64 битного режима 
     mov rsi, rsp       ;  rsp указывает на последнее значение в стеке т.е. на RAX ('W')                       
@@ -178,43 +88,7 @@ print_line:
     pop rax
     ret    
 
-section '.print_string' executable
-; | input
-; rax = адрес строки
-print_string:
-    push rax
-    push rdx
-    push rdi
-    push rsi
-    mov rsi, rax         ; адрес строки в rcx
-    call length_string
-    mov rdx, rax         ; длину строки в rdx
-    mov rax, 1         ; указание ос что будет запись
-    mov rdi, 1         ; в стандартный поток вывода (терминал)
-    syscall
-    pop rsi
-    pop rdi
-    pop rdx
-    pop rax
-    ret
 
-section '.length_string' executable
-; | input
-; rax = адрес строки
-; | output
-; rax length
-length_string:
-    push rdx    ; сохраняем rdx
-    xor rdx, rdx
-    .next_iter:
-        cmp [rax + rdx], byte 0
-        je .close
-        inc rdx
-        jmp .next_iter
-    .close:
-        mov rax, rdx    ; сохраняем длину строки в raz
-        pop rdx         ; востанавливаем rdx    
-        ret
 
 section '.exit' executable
 exit:
