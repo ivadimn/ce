8080ff1ac5b72d378c1980011f1bfca24acc1bd0
vadim <ivadimn@mail.ru>
2024-03-04 17:30:08 +0300

    create dirwalk proc

--- gfile/main.c
+++ gfile/main.c
@@ -22,6 +22,18 @@
 #define DIRSIZ 14
 #endif
 
+typedef enum {TYPE_DIR, TYPE_FILE} file_t;
+
+typedef struct file_info_t {
+    char name[MAX_PATH];
+    size_t size;
+    file_t type;
+} file_info_t;
+
+void get_file_info(const char* file, file_info_t* finfo) {
+
+}
+
 
 void dirwalk( char *dir,void (*fcn)(char *)){
 
@@ -39,7 +51,7 @@ void dirwalk( char *dir,void (*fcn)(char *)){
     while((dp=readdir(dfd)) != NULL){
         // Skip . and .. is redundant.
         
-        if(strcmp(dp->d_name,".") == 0
+        /*if(strcmp(dp->d_name,".") == 0
             || strcmp(dp->d_name,"..") ==0 )
             continue;
         if(strlen(dir)+strlen(dp->d_name)+2 > sizeof(name))
@@ -48,10 +60,14 @@ void dirwalk( char *dir,void (*fcn)(char *)){
             sprintf(name,"%s/%s",dir,dp->d_name);
             // Call fsize
             (*fcn)(name);
-        }
+        }*/
+        printf("%s/   %s\n",dir,dp->d_name);
     }
     closedir(dfd);
 }
+
+
+
 
 void fsize(char *name){
     struct stat stbuf;
@@ -66,13 +82,13 @@ void fsize(char *name){
     }
     struct passwd *pwd = getpwuid(stbuf.st_uid);
     //print file name,size and owner
-    printf("%81d %s Owner: %s\n",(int)stbuf.st_size,name,pwd->pw_name);
+    printf("%1d %s Owner: %s\n",(int)stbuf.st_size,name,pwd->pw_name);
 }
 
 int main(int argc,char *argv[]){
 
     if(argc==1)
-        fsize(".");
+        fsize("/home/vadim");
     else 
         while(--argc>0)
             fsize(*++argv);
