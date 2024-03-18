b517ba9419576c189a192f1c8a98959694f2583e
vadim <ivadimn@mail.ru>
2024-03-18 17:52:47 +0300

    update treeview

--- tv/main.c
+++ tv/main.c
@@ -10,11 +10,21 @@ enum
    N_COLUMNS
 };
 
-static void refresh_tree_model(GtkTreeStore *store, file_info_t* dir) {
+static void selection_changed(GtkTreeSelection *selection, gpointer data) {
+  GtkTreeIter iter;
+  GtkTreeModel *model;
+  gchar *name;
+  if (gtk_tree_selection_get_selected(selection, &model, &iter)) {
+    gtk_tree_model_get(model, &iter, NAME_COLUMN, &name, -1);
+    g_print ("You selected a file: %s\n", name);
+    g_free (name);  
+  }
+}
 
-  GtkTreeIter iter1;
-  GtkTreeIter iter2;
+static void refresh_tree_model(GtkTreeStore *store, file_info_t* dir, GtkTreeIter *parent_iter) {
 
+  GtkTreeIter iter;
+  
   gtk_window_set_title (GTK_WINDOW (window), dir->full_name);
 
   get_file_list(dir);
@@ -24,25 +34,12 @@ static void refresh_tree_model(GtkTreeStore *store, file_info_t* dir) {
   
   for (size_t i = 0; i < dir->size; i++) {
     printf(" - %s\n", dir->flist[i].name);
-    gtk_tree_store_append(store, &iter1, NULL);
-    gtk_tree_store_set(store, &iter1,
+    gtk_tree_store_append(store, &iter, NULL);
+    gtk_tree_store_set(store, &iter,
                       NAME_COLUMN, dir->flist[i].name,
                       SIZE_COLUMN, dir->flist[i].size, -1);
   }
 
-  /*gtk_tree_store_append(store, &iter2, &iter1);  
-  gtk_tree_store_set(store, &iter2,
-                      TITLE_COLUMN, "Volumn 1: Fundamental Algorithms",
-                      -1);                  
-  
-  gtk_tree_store_append(store, &iter2, &iter1);  
-  gtk_tree_store_set(store, &iter2,
-                      TITLE_COLUMN, "Volumn 2: Seminumerical Algorithms",
-                      -1);                  
-  gtk_tree_store_append(store, &iter2, &iter1);  
-  gtk_tree_store_set(store, &iter2,
-                      TITLE_COLUMN, "Volumn 3: Sorting and Searching",
-                      -1); */
 }
 
 static GtkWidget* init_tree(file_info_t* dir) {
@@ -50,33 +47,44 @@ static GtkWidget* init_tree(file_info_t* dir) {
   GtkWidget *tree;
   GtkTreeViewColumn *column;
   GtkCellRenderer *renderer;
+  GtkTreeSelection *select;
 
   store = gtk_tree_store_new(N_COLUMNS, 
                               G_TYPE_STRING,
                               G_TYPE_ULONG
                               );
 
-  refresh_tree_model(store, dir);
+  refresh_tree_model(store, dir, NULL);
 
   tree = gtk_tree_view_new_with_model(GTK_TREE_MODEL(store));
 
   g_object_unref(G_OBJECT(store));
-  renderer = gtk_cell_renderer_text_new();
+  renderer = gtk_cell_renderer_text_new();  
+  
   
   column = gtk_tree_view_column_new_with_attributes("File name", renderer, 
-                                                    "text", NAME_COLUMN, NULL);
+                                                    "text", NAME_COLUMN,  
+                                                    NULL);
+  //gtk_tree_view_column_set_expand(column, TRUE);
+  gtk_tree_view_column_set_resizable(column, TRUE);
   gtk_tree_view_append_column(GTK_TREE_VIEW(tree), column);
   
   column = gtk_tree_view_column_new_with_attributes("Size", renderer, 
-                                                    "text", SIZE_COLUMN, NULL);
+                                                    "text", SIZE_COLUMN, 
+                                                     NULL);
   gtk_tree_view_append_column(GTK_TREE_VIEW(tree), column);
+
+  select = gtk_tree_view_get_selection(GTK_TREE_VIEW(tree));
+  gtk_tree_selection_set_mode(select, GTK_SELECTION_SINGLE);
+  g_signal_connect(G_OBJECT(select), "changed", 
+                            G_CALLBACK (selection_changed), NULL);
   
   return tree;
 
 }
 
 
-static void activate (GtkApplication* app, gpointer user_data)   {
+void activate (GtkApplication* app, gpointer user_data)   {
 
   GtkWidget *tree;
   file_info_t* dir;
@@ -103,9 +111,7 @@ int main(int argc,char *argv[]){
   GtkApplication *app;
   int status;
   
-
-
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  app = gtk_application_new ("gtk.treeview.example", G_APPLICATION_FLAGS_NONE);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
